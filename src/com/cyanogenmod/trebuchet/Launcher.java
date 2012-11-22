68cc9fa0fff30192f01fffb1898453d2d49ec268
Paul Henschel <drcmda@gmail.com>
2012-11-22 02:10:42 +0100

    Appdrawer color transition
    Change-Id: I8a1480df9cc379f5cde826fcfb2ce44bc0df1286

--- src/com/cyanogenmod/trebuchet/Launcher.java
+++ src/com/cyanogenmod/trebuchet/Launcher.java
@@ -73,6 +73,7 @@ import android.text.Selection;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 import android.text.method.TextKeyListener;
+import android.util.ExtendedPropertiesUtils;
 import android.util.Log;
 import android.view.Display;
 import android.view.HapticFeedbackConstants;
@@ -380,6 +381,7 @@ public final class Launcher extends Activity
         }
 
         super.onCreate(savedInstanceState);
+
         LauncherApplication app = ((LauncherApplication)getApplication());
         mSharedPrefs = getSharedPreferences(LauncherApplication.getSharedPreferencesKey(),
                 Context.MODE_PRIVATE);
@@ -2184,6 +2186,37 @@ public final class Launcher extends Activity
         }
     }
 
+    /**
+     * Event handler for the "grid" button that appears on the home screen, which
+     * enters all apps mode.
+     *
+     * @param v The view that was clicked.
+     */
+    public void onClickAllAppsButton(View v) {
+        if (mOnResumeState == State.NONE)
+        {
+            // Fade bars back to default in 250ms
+            for (int i=ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR;
+                i <= ExtendedPropertiesUtils.PARANOID_COLORS_STATBAR; i++) {
+                String mSetting = Settings.System.getString(getContentResolver(),
+                    ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i]);
+                String[] mColors = (mSetting == null || mSetting.equals("") ?
+                    ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i] :
+                    mSetting).split(ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+                Settings.System.putString(getContentResolver(),
+                    ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i], 
+                    mColors[0] + "|" + ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i].split(
+                    ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER)[0] + "|1|250");
+            }
+        }
+        showAllApps(true);
+    }
+
+    public void onTouchDownAllAppsButton(View v) {
+        // Provide the same haptic feedback that the system offers for virtual keys.
+        v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+    }
+
     public void onClickAppMarketButton(View v) {
         if (mAppMarketIntent != null) {
             startActivitySafely(v, mAppMarketIntent, "app market");
@@ -3047,6 +3080,26 @@ public final class Launcher extends Activity
     }
 
     void showWorkspace(boolean animated) {
+        if (mOnResumeState == State.NONE)
+        {
+            // Fade bars back to Launcher-color in 250ms
+            String[] launcherColors = ExtendedPropertiesUtils.getProperty("com.android.launcher" +
+                ExtendedPropertiesUtils.PARANOID_COLORS_SUFFIX).split(ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+            for (int i=ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR;
+                i <= ExtendedPropertiesUtils.PARANOID_COLORS_STATBAR; i++) {
+                String mSetting = Settings.System.getString(getContentResolver(),
+                    ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i]);
+                String[] mColors = (mSetting == null || mSetting.equals("") ?
+                    ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i] :
+                    mSetting).split(ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+                String newColor = launcherColors.length == ExtendedPropertiesUtils.PARANOID_COLORS_COUNT ? 
+                    launcherColors[i] : ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i].split(
+                    ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER)[0];
+                Settings.System.putString(getContentResolver(),
+                    ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i], 
+                    mColors[0] + "|" + newColor + "|1|250");
+            }
+        }
         showWorkspace(animated, null);
     }
 
